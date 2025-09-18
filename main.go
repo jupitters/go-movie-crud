327ec52b4add9903dcc5f50ce861126adae35b2c
jupitters <sampaiobale@gmail.com>
2025-09-18 10:47:16 -0300

    defining endpoints and methods

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -15,4 +22,13 @@ type Director struct {
 func main() {
 	movies := []Movie{}
 
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
