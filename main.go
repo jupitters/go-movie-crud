59d34a454d480275b66fd0c964d208aca2d900c7
jupitters <sampaiobale@gmail.com>
2025-09-18 10:48:26 -0300

    models file

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
-}
-
-type Director struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
-
 func main() {
 	movies := []Movie{}
 
